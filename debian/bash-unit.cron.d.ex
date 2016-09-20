e46243dbefbe3bad0d60e32ac5db5f6cd61b593b
Pascal Grange <pascal@grange.nom.fr>
2016-09-20 18:44:56 +0200

    Cleaning automatically generated debian files.

--- debian/bash-unit.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the bash-unit package
-#
-0 4	* * *	root	[ -x /usr/bin/bash-unit_maintenance ] && /usr/bin/bash-unit_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(bash-unit):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="bash-unit" command="/usr/bin/bash-unit"
